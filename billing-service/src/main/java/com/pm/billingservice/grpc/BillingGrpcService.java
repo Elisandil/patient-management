515e2f34dfb42560efc2248999c679ec6f90d4d6
aog-developer <elisandil88@gmail.com>
2025-08-27 05:52:13 +0200

    feat: added new module - BillingService

---
+++ billing-service/src/main/java/com/pm/billingservice/grpc/BillingGrpcService.java
@@ -0,0 +1,16 @@
+package com.pm.billingservice.grpc;
+
+import billing.BillingResponse;
+import billing.BillingServiceGrpc.BillingServiceImplBase;
+import io.grpc.stub.StreamObserver;
+import net.devh.boot.grpc.server.service.GrpcService;
+
+@GrpcService
+public class BillingGrpcService extends BillingServiceImplBase {
+    @Override
+    public void createBillingAccount(billing.BillingRequest billingRequest,
+                    StreamObserver<BillingResponse> responseObserver) {
+
+
+    }
+}
